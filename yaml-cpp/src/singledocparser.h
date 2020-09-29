449b89c31b8cd3faff864b34796229d4ae7edcbb
Kristian Ivarsson <kristone@hotmail.com>
2020-09-29 16:57:36 +0200

    Updated yaml-cpp to 0.6.3

--- yaml-cpp/src/singledocparser.h
+++ yaml-cpp/src/singledocparser.h
@@ -1,65 +1,69 @@
 #ifndef SINGLEDOCPARSER_H_62B23520_7C8E_11DE_8A39_0800200C9A66
 #define SINGLEDOCPARSER_H_62B23520_7C8E_11DE_8A39_0800200C9A66
 
-#if defined(_MSC_VER) || (defined(__GNUC__) && (__GNUC__ == 3 && __GNUC_MINOR__ >= 4) || (__GNUC__ >= 4)) // GCC supports "pragma once" correctly since 3.4
+#if defined(_MSC_VER) ||                                            \
+    (defined(__GNUC__) && (__GNUC__ == 3 && __GNUC_MINOR__ >= 4) || \
+     (__GNUC__ >= 4))  // GCC supports "pragma once" correctly since 3.4
 #pragma once
 #endif
 
-
-#include "yaml-cpp/anchor.h"
-#include "yaml-cpp/noncopyable.h"
-#include <string>
 #include <map>
 #include <memory>
+#include <string>
 
-namespace YAML
-{
-	struct Directives;
-	struct Mark;
-	struct Token;
-	class CollectionStack;
-	class EventHandler;
-	class Node;
-	class Scanner;
-	
-	class SingleDocParser: private noncopyable
-	{
-	public:
-		SingleDocParser(Scanner& scanner, const Directives& directives);
-		~SingleDocParser();
+#include "yaml-cpp/anchor.h"
 
-		void HandleDocument(EventHandler& eventHandler);
+namespace YAML {
+class CollectionStack;
+class EventHandler;
+class Node;
+class Scanner;
+struct Directives;
+struct Mark;
+struct Token;
 
-	private:
-		void HandleNode(EventHandler& eventHandler);
-		
-		void HandleSequence(EventHandler& eventHandler);
-		void HandleBlockSequence(EventHandler& eventHandler);
-		void HandleFlowSequence(EventHandler& eventHandler);
-		
-		void HandleMap(EventHandler& eventHandler);
-		void HandleBlockMap(EventHandler& eventHandler);
-		void HandleFlowMap(EventHandler& eventHandler);
-		void HandleCompactMap(EventHandler& eventHandler);
-		void HandleCompactMapWithNoKey(EventHandler& eventHandler);
-		
-		void ParseProperties(std::string& tag, anchor_t& anchor);
-		void ParseTag(std::string& tag);
-		void ParseAnchor(anchor_t& anchor);
-		
-		anchor_t RegisterAnchor(const std::string& name);
-		anchor_t LookupAnchor(const Mark& mark, const std::string& name) const;
-		
-	private:
-		Scanner& m_scanner;
-		const Directives& m_directives;
-		std::auto_ptr<CollectionStack> m_pCollectionStack;
-		
-		typedef std::map<std::string, anchor_t> Anchors;
-		Anchors m_anchors;
-		
-		anchor_t m_curAnchor;
-	};
-}
+class SingleDocParser {
+ public:
+  SingleDocParser(Scanner& scanner, const Directives& directives);
+  SingleDocParser(const SingleDocParser&) = delete;
+  SingleDocParser(SingleDocParser&&) = delete;
+  SingleDocParser& operator=(const SingleDocParser&) = delete;
+  SingleDocParser& operator=(SingleDocParser&&) = delete;
+  ~SingleDocParser();
 
-#endif // SINGLEDOCPARSER_H_62B23520_7C8E_11DE_8A39_0800200C9A66
+  void HandleDocument(EventHandler& eventHandler);
+
+ private:
+  void HandleNode(EventHandler& eventHandler);
+
+  void HandleSequence(EventHandler& eventHandler);
+  void HandleBlockSequence(EventHandler& eventHandler);
+  void HandleFlowSequence(EventHandler& eventHandler);
+
+  void HandleMap(EventHandler& eventHandler);
+  void HandleBlockMap(EventHandler& eventHandler);
+  void HandleFlowMap(EventHandler& eventHandler);
+  void HandleCompactMap(EventHandler& eventHandler);
+  void HandleCompactMapWithNoKey(EventHandler& eventHandler);
+
+  void ParseProperties(std::string& tag, anchor_t& anchor,
+                       std::string& anchor_name);
+  void ParseTag(std::string& tag);
+  void ParseAnchor(anchor_t& anchor, std::string& anchor_name);
+
+  anchor_t RegisterAnchor(const std::string& name);
+  anchor_t LookupAnchor(const Mark& mark, const std::string& name) const;
+
+ private:
+  Scanner& m_scanner;
+  const Directives& m_directives;
+  std::unique_ptr<CollectionStack> m_pCollectionStack;
+
+  typedef std::map<std::string, anchor_t> Anchors;
+  Anchors m_anchors;
+
+  anchor_t m_curAnchor;
+};
+}  // namespace YAML
+
+#endif  // SINGLEDOCPARSER_H_62B23520_7C8E_11DE_8A39_0800200C9A66
